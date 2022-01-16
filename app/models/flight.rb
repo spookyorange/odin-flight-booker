00746bc32737f8327badf5a952220d3c5aa1e850
spookyorange <korkmazcoltu@mail.ru>
2022-01-16 15:25:46 +0300

    set up of associations between flight booking and passenger

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+  has_many :bookings
+  has_many :passengers, through: :bookings
 
   validates :departure_airport_id, presence: true
   validates :arrival_airport_id, presence: true
